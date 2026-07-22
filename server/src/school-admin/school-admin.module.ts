4a26d752ea0921183035282dd0ab46473fb9aae4
柚子 <linfanyes@163.com>
2026-07-22 22:41:50 +0800

    fix: SchoolAdminModule 缺少 ClassItem 的 TypeORM 注册，导致 NestJS DI 失败

--- server/src/school-admin/school-admin.module.ts
+++ server/src/school-admin/school-admin.module.ts
@@ -4,11 +4,12 @@ import { SchoolAdmin } from './school-admin.entity'
 import { User } from '../users/user.entity'
 import { Student } from '../students/student.entity'
 import { School } from '../school/school.entity'
+import { ClassItem } from '../classes/class.entity'
 import { SchoolAdminService } from './school-admin.service'
 import { SchoolAdminController } from './school-admin.controller'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SchoolAdmin, User, Student, School])],
+  imports: [TypeOrmModule.forFeature([SchoolAdmin, User, Student, School, ClassItem])],
   providers: [SchoolAdminService],
   controllers: [SchoolAdminController],
   exports: [SchoolAdminService],
